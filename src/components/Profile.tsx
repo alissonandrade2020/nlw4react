99d4dcc44a2cd9f3672b52cd3b65e049b3b71b70
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2021-02-23 14:27:20 -0300

    2º Dia

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+    return (
+       <div className={styles.profileContainer}>
+           <img src="https://media-exp1.licdn.com/dms/image/C5603AQF5AHeDK53RIw/profile-displayphoto-shrink_100_100/0/1567017195629?e=1619654400&v=beta&t=CyJ89sI1eVcRsGN-VvJYz8n9rVvu0C7DAAkGAVazaps" alt="Alisson de Andrade Araújo"/>
+           <div>
+               <strong>Alisson de Andrade</strong>
+               <p><a href="https://github.com/alissonandrade2020"><b>Acesse o Github</b></a></p>
+               <p><a href="https://app.rocketseat.com.br/me/alissondeandradearaujo"><b>Acesse o Perfil - Rocketseat</b></a></p>
+               <p>
+                   <img src="icons/level.svg" alt="Level"/>
+                   Level 1</p>
+           </div>
+       </div>
+    );
+}
