187793e20c04ce51f25ecce78047e94d0099ffe4
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2021-02-24 14:09:22 -0300

    3º Dia

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect} from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown(){
     const [time, setTime] = useState(12 * 60);
-    const [active, setActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -12,16 +15,25 @@ export function Countdown(){
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown(){
-        setActive(true)
+        setIsActive(true)
+    }
+
+    function resetCountdown() {
+        clearTimeout(countdownTimeout)
+        setIsActive(false);
+        setTime(12 * 60);
     }
 
     useEffect(() => {
-        if(active && time > 0){
-            setTimeout(() => {
+        if(isActive && time > 0){
+            countdownTimeout = setTimeout(() => {
                 setTime(time - 1)
             }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true);
+            setIsActive(false);
         }
-    }, [active, time])
+    }, [isActive, time])
 
     return (
         <div>
@@ -36,7 +48,43 @@ export function Countdown(){
                 <span> {secondRight} </span>
             </div>            
           </div>
-          <button type="button" className={styles.startCountdownButton} onClick={startCountdown}>Inicio</button>
+
+          {hasFinished ? (
+              <button 
+              disabled
+              className={styles.startCountdownButton} 
+              >
+               Ciclo encerrado!
+              </button>
+          ) : (
+        <>
+            {isActive ? (
+            <button 
+            type="button" 
+            className={`${styles.startCountdownButton} ${styles.startCountdownButtonActive}`} 
+            onClick={resetCountdown}
+            >
+             Abandonar ciclo
+            </button>
+          
+          ) : (
+
+            <button 
+            type="button" 
+            className={styles.startCountdownButton} 
+            onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+         )}         
+        
+        </>
+
+
+          )}
+              
         </div>
+
+        
     );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,7 @@ export function Profile(){
        <div className={styles.profileContainer}>
            <img src="https://media-exp1.licdn.com/dms/image/C5603AQF5AHeDK53RIw/profile-displayphoto-shrink_100_100/0/1567017195629?e=1619654400&v=beta&t=CyJ89sI1eVcRsGN-VvJYz8n9rVvu0C7DAAkGAVazaps" alt="Alisson de Andrade Araújo"/>
            <div>
-               <strong>Alisson de Andrade</strong>
+               <strong>Alisson Andrade</strong>
                <p><a href="https://github.com/alissonandrade2020"><b>Acesse o Github</b></a></p>
                <p><a href="https://app.rocketseat.com.br/me/alissondeandradearaujo"><b>Acesse o Perfil - Rocketseat</b></a></p>
                <p>
